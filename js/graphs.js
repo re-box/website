47cbbf50f9606789d2b03a6b349db1429137f62f
re-box <73078681+re-box@users.noreply.github.com>
2020-10-24 21:09:31 +0300

    Add files via upload

---
+++ js/graphs.js
@@ -0,0 +1,245 @@
+
+var ctxP = document.getElementById("labelChart").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+
+    type: 'pie',
+    data: {
+      labels: ["Yes", "No"],
+      datasets: [{
+        data: [97.6,2.4],
+        backgroundColor: ["#3bbbe2", "#ef3930"],
+        hoverBackgroundColor: ["#3bbbe2", "#ef3930"]
+    }]
+},
+options: {
+  responsive: true,
+  legend: {
+    position: 'right',
+    labels: {
+      padding: 20,
+      boxWidth: 10
+    }
+   },
+   animation:{
+      duration: 2000,
+      easing:'easeOutBounce'
+   }
+}
+});
+
+var ctxP = document.getElementById("labelChart2").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+
+    type: 'pie',
+    data: {
+      labels: ["Yes", "No"],
+      datasets: [{
+        data: [50.4,49.6],
+        backgroundColor: ["#3bbbe2", "#ef3930"],
+        hoverBackgroundColor: ["#3bbbe2", "#ef3930"]
+    }]
+},
+options: {
+  responsive: true,
+  legend: {
+    position: 'right',
+    labels: {
+      padding: 20,
+      boxWidth: 10
+    }
+   },
+   animation:{
+      duration: 2000,
+      easing:'easeOutBounce'
+   }
+}});
+var ctxP = document.getElementById("labelChart3").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+
+    type: 'pie',
+    data: {
+      labels: ["Yes", "No"],
+      datasets: [{
+        data: [50.4,49.6],
+        backgroundColor: ["#3bbbe2", "#ef3930"],
+        hoverBackgroundColor: ["#3bbbe2", "#ef3930"]
+    }]
+},
+options: {
+  responsive: true,
+  legend: {
+    position: 'right',
+    labels: {
+      padding: 20,
+      boxWidth: 10
+    }
+   },
+   animation:{
+      duration: 2000,
+      easing:'easeOutBounce'
+   }
+}
+});
+var ctxP = document.getElementById("labelChart4").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+
+    type: 'pie',
+    data: {
+      labels: ["Yes", "No"],
+      datasets: [{
+        data: [38.4,61.6],
+        backgroundColor: ["#3bbbe2", "#ef3930"],
+        hoverBackgroundColor: ["#3bbbe2", "#ef3930"]
+    }]
+},
+options: {
+  responsive: true,
+  legend: {
+    position: 'right',
+    labels: {
+      padding: 20,
+      boxWidth: 10
+    }
+   },
+   animation:{
+      duration: 2000,
+      easing:'easeOutBounce'
+   }
+}
+});
+var ctxP = document.getElementById("labelChart5").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+    type: 'pie',
+    data: {
+      labels: ["Yes", "No"],
+      datasets: [{
+        data: [48,52],
+        backgroundColor: ["#3bbbe2", "#ef3930"],
+        hoverBackgroundColor: ["#3bbbe2", "#ef3930"]
+    }]
+},
+options: {
+  responsive: true,
+  legend: {
+    position: 'right',
+    labels: {
+      padding: 20,
+      boxWidth: 10
+    }
+   },
+   animation:{
+      duration: 2000,
+      easing:'easeOutBounce'
+   }
+}
+});
+var ctxP = document.getElementById("labelChart6").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+    type: 'pie',
+    data: {
+        labels: ["Metal", "Plastic","Paper","Batteries","Clothing","Glass","Oil"],
+        datasets: [{
+            data: [1,36,35.2,11.2,9.6,1,5],
+            backgroundColor: ["#ef3930", "#3bbbe2","#ffca3c","#f09c28","#8d40c0","#82b630","#454859"],
+            hoverBackgroundColor: ["#ef3930", "#3bbbe2","#ffca3c","#f09c28","#8d40c0","#82b630","#454859"]
+        }]
+    },
+options: {
+  responsive: true,
+  legend: {
+    position: 'right',
+    labels: {
+      padding: 20,
+      boxWidth: 10
+    }
+   },
+   animation:{
+      duration: 2000,
+      easing:'easeOutBounce'
+   }
+}
+});
+
+
+
+var ctxP = document.getElementById("labelChart7").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+
+    type: 'pie',
+    data: {
+      labels: ["Yes", "No"],
+      datasets: [{
+        data: [90.4,9.6],
+        backgroundColor: ["#3bbbe2", "#ef3930"],
+        hoverBackgroundColor: ["#3bbbe2", "#ef3930"]
+    }]
+},
+options: {
+  responsive: true,
+  legend: {
+    position: 'right',
+    labels: {
+      padding: 20,
+      boxWidth: 10
+    }
+   },
+   animation:{
+      duration: 2000,
+      easing:'easeOutBounce'
+   }
+}
+});
+var ctxP = document.getElementById("labelChart8").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+
+    type: 'pie',
+    data: {
+      labels: ["Yes", "No"],
+      datasets: [{
+        data: [54.4,45.6],
+        backgroundColor: ["#3bbbe2", "#ef3930"],
+        hoverBackgroundColor: ["#3bbbe2", "#ef3930"]
+    }]
+},
+options: {
+  responsive: true,
+  legend: {
+    position: 'right',
+    labels: {
+      padding: 20,
+      boxWidth: 10
+    }
+   },
+   animation:{
+      duration: 2000,
+      easing:'easeOutBounce'
+   }
+}
+});
+var ctxP = document.getElementById("labelChart9").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+
+    type: 'pie',
+    data: {
+      labels: ["Yes", "No"],
+      datasets: [{
+        data: [5.6,94.4],
+        backgroundColor: ["#3bbbe2", "#ef3930"],
+        hoverBackgroundColor: ["#3bbbe2", "#ef3930"]
+    }]
+},
+options: {
+  responsive: true,
+  legend: {
+    position: 'right',
+    labels: {
+      padding: 20,
+      boxWidth: 10
+    }
+   },
+   animation:{
+      duration: 2000,
+      easing:'easeOutBounce'
+   }
+}
+});
